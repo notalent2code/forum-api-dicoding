19f819e4275ac49c744e861c40d23c1a01842def
notalent2code <rihlanrihlan24@gmail.com>
2023-09-17 13:29:09 +0700

    feat: new comment entity

---
+++ src/Domains/threads/comments/entities/_test/NewComment.test.js
@@ -0,0 +1,31 @@
+const NewComment = require('../NewComment');
+
+describe('a NewComment entity', () => {
+  it('should throw error when payload did not contain needed property', () => {
+    const payload = {};
+
+    expect(() => new NewComment(payload)).toThrowError(
+      'NEW_COMMENT.NOT_CONTAIN_NEEDED_PROPERTY'
+    );
+  });
+
+  it('should throw error when payload did not meet data type specification', () => {
+    const payload = {
+      content: 123,
+    };
+
+    expect(() => new NewComment(payload)).toThrowError(
+      'NEW_COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION'
+    );
+  });
+
+  it('should create Comment object correctly', () => {
+    const payload = {
+      content: 'A comment',
+    };
+
+    const newComment = new NewComment(payload);
+
+    expect(newComment.content).toEqual(payload.content);
+  });
+});
