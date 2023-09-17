1b3aa21db0981cfc483282c067f7fda8bc8b7f8c
notalent2code <rihlanrihlan24@gmail.com>
2023-09-17 13:45:59 +0700

    refactor: auth interface

--- src/Interfaces/http/api/authentications/index.js
+++ src/Interfaces/http/api/authentications/index.js
@@ -1,10 +1,12 @@
 const routes = require('./routes');
 const AuthenticationsHandler = require('./handler');
 
-module.exports = {
+const authentications = {
   name: 'authentications',
   register: async (server, { container }) => {
     const authenticationsHandler = new AuthenticationsHandler(container);
     server.route(routes(authenticationsHandler));
   },
 };
+
+module.exports = authentications;
