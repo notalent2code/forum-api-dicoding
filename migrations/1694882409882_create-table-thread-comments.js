f78d4a93396a5c6edade7bc451d9c5461e11f522
notalent2code <rihlanrihlan24@gmail.com>
2023-09-17 13:24:41 +0700

    feat: create table thread comments migration

---
+++ migrations/1694882409882_create-table-thread-comments.js
@@ -0,0 +1,36 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable('thread_comments', {
+    id: { type: 'VARCHAR(50)', primaryKey: true },
+    thread_id: { type: 'VARCHAR(50)', notNull: true },
+    content: { type: 'TEXT', notNull: true },
+    owner: { type: 'VARCHAR(50)', notNull: true },
+    is_deleted: {
+      type: 'BOOLEAN',
+      notNull: true,
+      default: pgm.func('FALSE'),
+    },
+    date: {
+      type: 'TIMESTAMPTZ',
+      notNull: true,
+      default: pgm.func('CURRENT_TIMESTAMP'),
+    },
+  });
+
+  pgm.addConstraint(
+    'thread_comments',
+    'fk_thread_comments.thread_id_threads.id',
+    'FOREIGN KEY (thread_id) REFERENCES threads (id) ON DELETE CASCADE'
+  );
+
+  pgm.addConstraint(
+    'thread_comments',
+    'fk_thread_comments.owner_users.id',
+    'FOREIGN KEY (owner) REFERENCES users (id) ON DELETE CASCADE'
+  );
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('thread_comments');
+};
