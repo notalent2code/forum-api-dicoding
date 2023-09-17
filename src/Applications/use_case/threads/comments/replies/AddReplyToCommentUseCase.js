3687560de15ea4b719f669b592c05aae330de706
notalent2code <rihlanrihlan24@gmail.com>
2023-09-17 13:26:55 +0700

    feat: add reply to comment use case

---
+++ src/Applications/use_case/threads/comments/replies/AddReplyToCommentUseCase.js
@@ -0,0 +1,31 @@
+const NewReply = require('../../../../../Domains/threads/comments/replies/entities/NewReply');
+
+class AddReplyToCommentUseCase {
+  constructor({
+    threadCommentRepliesRepository,
+    threadCommentsRepository,
+    threadsRepository,
+  }) {
+    this._threadCommentRepliesRepository = threadCommentRepliesRepository;
+    this._threadCommentsRepository = threadCommentsRepository;
+    this._threadsRepository = threadsRepository;
+  }
+
+  async execute(threadId, commentId, payload, owner) {
+    const newReply = new NewReply(payload);
+
+    await this._threadsRepository.verifyThread(threadId);
+    await this._threadCommentsRepository.verifyCommentLocation(
+      commentId,
+      threadId
+    );
+
+    return this._threadCommentRepliesRepository.addReplyToComment(
+      commentId,
+      newReply,
+      owner
+    );
+  }
+}
+
+module.exports = AddReplyToCommentUseCase;
