d17fa8b9ca4351c1ba3d697afd9929d62fa602ec
notalent2code <rihlanrihlan24@gmail.com>
2023-09-17 13:26:30 +0700

    feat: like or dislike use case

---
+++ src/Applications/use_case/threads/comments/LikeOrDislikeCommentUseCase.js
@@ -0,0 +1,33 @@
+class LikeOrDislikeCommentUsecase {
+  constructor({
+    threadsRepository,
+    threadCommentsRepository,
+    threadCommentLikesRepository,
+  }) {
+    this._threadsRepository = threadsRepository;
+    this._threadCommentsRepository = threadCommentsRepository;
+    this._threadCommentLikesRepository = threadCommentLikesRepository;
+  }
+
+  async execute(threadId, commentId, liker) {
+    await this._threadsRepository.verifyThread(threadId);
+    await this._threadCommentsRepository.verifyCommentLocation(
+      commentId,
+      threadId
+    );
+
+    const isLiked = await this._threadCommentLikesRepository.verifyCommentLike(
+      commentId,
+      liker
+    );
+
+    if (isLiked) {
+      this._threadCommentLikesRepository.dislikeAComment(commentId, liker);
+      return;
+    }
+
+    this._threadCommentLikesRepository.likeAComment(commentId, liker);
+  }
+}
+
+module.exports = LikeOrDislikeCommentUsecase;
