18aca0eb7e0b03aa9118c180fb5a671729002a7f
notalent2code <rihlanrihlan24@gmail.com>
2023-09-17 13:26:12 +0700

    feat: add comment to thread use case

---
+++ src/Applications/use_case/threads/comments/AddCommentToThreadUseCase.js
@@ -0,0 +1,22 @@
+const NewComment = require('../../../../Domains/threads/comments/entities/NewComment');
+
+class AddCommentToThreadUseCase {
+  constructor({ threadCommentsRepository, threadsRepository }) {
+    this._threadCommentsRepository = threadCommentsRepository;
+    this._threadsRepository = threadsRepository;
+  }
+
+  async execute(threadId, payload, owner) {
+    const newComment = new NewComment(payload);
+
+    await this._threadsRepository.verifyThread(threadId);
+
+    return this._threadCommentsRepository.addCommentToThread(
+      threadId,
+      newComment,
+      owner
+    );
+  }
+}
+
+module.exports = AddCommentToThreadUseCase;
