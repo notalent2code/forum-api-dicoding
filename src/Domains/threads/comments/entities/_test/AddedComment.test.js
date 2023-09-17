92c6bdc9a617575fedb640c28114f0bd9e4794ef
notalent2code <rihlanrihlan24@gmail.com>
2023-09-17 13:28:50 +0700

    feat: added comment entity

---
+++ src/Domains/threads/comments/entities/_test/AddedComment.test.js
@@ -0,0 +1,33 @@
+const AddedComment = require('../AddedComment')
+
+describe('a AddedComment entities', () => {
+  it('should throw error when payload did not contain needed property', () => {
+    const payload = { }
+
+    expect(() => new AddedComment(payload)).toThrowError('ADDED_COMMENT.NOT_CONTAIN_NEEDED_PROPERTY')
+  })
+
+  it('should throw error when payload did not meet data type specification', () => {
+    const payload = {
+      id: 123,
+      content: 'dicoding',
+      owner: {}
+    }
+
+    expect(() => new AddedComment(payload)).toThrowError('ADDED_COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION')
+  })
+
+  it('should create AddedComment object correctly', () => {
+    const payload = {
+      id: 'user-123',
+      content: 'dicoding',
+      owner: 'user-123'
+    }
+
+    const addedComment = new AddedComment(payload)
+
+    expect(addedComment.id).toStrictEqual(payload.id)
+    expect(addedComment.content).toStrictEqual(payload.content)
+    expect(addedComment.owner).toStrictEqual(payload.owner)
+  })
+})
