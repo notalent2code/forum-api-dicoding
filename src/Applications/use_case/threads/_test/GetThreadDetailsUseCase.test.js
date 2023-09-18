75a697386a791eb3864d93cb2f96371d1162344d
notalent2code <rihlanrihlan24@gmail.com>
2023-09-18 13:48:19 +0700

    fix: detail thread test

--- src/Applications/use_case/threads/_test/GetThreadDetailsUseCase.test.js
+++ src/Applications/use_case/threads/_test/GetThreadDetailsUseCase.test.js
@@ -7,9 +7,12 @@ const Comment = require('../../../../Domains/threads/comments/entities/Comment')
 const ThreadCommentRepliesRepository = require('../../../../Domains/threads/comments/replies/ThreadCommentRepliesRepository');
 
 const GetThreadDetailsUseCase = require('../GetThreadDetailsUseCase');
+const Reply = require('../../../../Domains/threads/comments/replies/entities/Reply');
 
 describe('GetThreadDetailsUseCase', () => {
   it('should orchestracting the get thread details by thread id action correctly', async () => {
+    const mockPayload = 'thread-123';
+
     const mockThread = new Thread({
       id: 'thread-123',
       title: 'a thread',
@@ -72,11 +75,61 @@ describe('GetThreadDetailsUseCase', () => {
       },
     ];
 
-    // mockRawReplies.forEach((reply) => {
-    //   Object.setPrototypeOf(reply, Object.prototype);
-    // });
-
-    // console.log('mockRawReplies', mockRawReplies)
+    const expectedThreadDetails = {
+      id: 'thread-123',
+      title: 'a thread',
+      body: 'a thread body',
+      date: new Date('2023-09-18T03:32:42.000Z'),
+      username: 'dicoding',
+      comments: [
+        {
+          id: 'comment-123',
+          username: 'dicoding',
+          date: new Date('2023-09-18T03:32:42.000Z'),
+          content: 'a comment',
+          likeCount: 0,
+          replies: [
+            new Reply({
+              id: 'reply-123',
+              username: 'dicoding',
+              date: new Date('2023-09-18T03:32:42.000Z'),
+              content: 'a reply',
+              isDeleted: false,
+            }),
+            new Reply({
+              id: 'reply-xyz',
+              username: 'dicoding',
+              date: new Date('2023-09-18T03:32:42.000Z'),
+              content: 'a reply',
+              isDeleted: true,
+            }),
+          ],
+        },
+        {
+          id: 'comment-xyz',
+          username: 'koseki_biboo',
+          date: new Date('2023-09-18T03:32:42.000Z'),
+          content: '**komentar telah dihapus**',
+          likeCount: 0,
+          replies: [
+            new Reply({
+              id: 'reply-ijk',
+              username: 'koseki_biboo',
+              date: new Date('2023-09-18T03:32:42.000Z'),
+              content: 'a reply',
+              isDeleted: false,
+            }),
+            new Reply({
+              id: 'reply-dummy',
+              username: 'dicoding',
+              date: new Date('2023-09-18T03:32:42.000Z'),
+              content: '**balasan telah dihapus**',
+              isDeleted: true,
+            }),
+          ],
+        },
+      ],
+    };
 
     const mockThreadsRepository = new ThreadsRepository();
     const mockThreadCommentsRepository = new ThreadCommentsRepository();
@@ -99,66 +152,12 @@ describe('GetThreadDetailsUseCase', () => {
       threadCommentRepliesRepository: mockThreadCommentRepliesRepository,
     });
 
-    const threadDetails = await usecase.execute('thread-123');
-
-    console.log('threadDetails', threadDetails[0]);
-
-    const expectedThreadDetails = {
-      id: 'thread-123',
-      title: 'a thread',
-      body: 'a thread body',
-      date: new Date('2023-09-18T03:32:42.000Z'),
-      username: 'dicoding',
-      comments: [
-        {
-          id: 'comment-123',
-          username: 'dicoding',
-          date: new Date('2023-09-18T03:32:42.000Z'),
-          content: 'a comment',
-          likeCount: 0,
-          replies: [
-            {
-              id: 'reply-123',
-              username: 'dicoding',
-              date: new Date('2023-09-18T03:32:42.000Z'),
-              content: 'a reply',
-            },
-            {
-              id: 'reply-xyz',
-              username: 'dicoding',
-              date: new Date('2023-09-18T03:32:42.000Z'),
-              content: '**balasan telah dihapus**',
-            },
-          ],
-        },
-        {
-          id: 'comment-xyz',
-          username: 'koseki_biboo',
-          date: new Date('2023-09-18T03:32:42.000Z'),
-          content: '**komentar telah dihapus**',
-          likeCount: 0,
-          replies: [
-            {
-              id: 'reply-ijk',
-              username: 'koseki_biboo',
-              date: new Date('2023-09-18T03:32:42.000Z'),
-              content: 'a reply',
-            },
-            {
-              id: 'reply-dummy',
-              username: 'dicoding',
-              date: new Date('2023-09-18T03:32:42.000Z'),
-              content: '**balasan telah dihapus**',
-            },
-          ],
-        },
-      ],
-    };
+    const threadDetails = await usecase.execute(mockPayload);
 
     expect(threadDetails).toStrictEqual(expectedThreadDetails);
-    expect(mockThreadsRepository.getThreadById).toBeCalledWith('thread-123');
+    expect(mockThreadsRepository.getThreadById).toBeCalledWith(mockPayload);
     expect(mockThreadCommentsRepository.getCommentsFromThread).toBeCalledWith(
-      'thread-123'
+      mockPayload
     );
     expect(
       mockThreadCommentRepliesRepository.getRawRepliesFromComments
