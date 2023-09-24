3f69ea22a9553500e19a0c961e6adb9c6bc8ece5
notalent2code <rihlanrihlan24@gmail.com>
2023-09-24 17:25:22 +0700

    failed cd scenario

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const container = require('./Infrastructures/container');
 const bootstrap = async () => {
   const server = await createServer(container);
   await server.start();
-  console.log(`Server running at ${server.info.uri}`);
+  console.log(`Server running on ${server.info.uri}`);
 };
 
 bootstrap();
